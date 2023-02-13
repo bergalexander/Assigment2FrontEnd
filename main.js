c60f8bd0573bb981db3a1dd19dc79460328c0e91
Henrik1995a@live.se <henrik1995a@live.se>
2023-02-13 12:23:30 +0100

    123

--- main.js
+++ main.js
@@ -1,132 +1,159 @@
-let form = document.querySelector('.search_pictuers');
-let list = document.querySelector('#respond')
+let form = document.querySelector(".search_pictuers");
+let list = document.querySelector("#respond");
 let nextPage = document.querySelector("#next");
 let previousPage = document.querySelector("#previous");
 
-let url = 'https://pixabay.com/api/?key=33506024-4219960d4e66c6a76dff1b306&q='
+let url = "https://pixabay.com/api/?key=33506024-4219960d4e66c6a76dff1b306&q=";
 let page = 1;
-form.onsubmit = async event => {
 
-    event.preventDefault();
+form.onsubmit = async (event) => {
+  event.preventDefault();
 
-    RemoveOldSearch();
+  RemoveOldSearch();
 
-    jsonData = await FetchData(form, url);
-    DisplayData(jsonData);
+  jsonData = await FetchData(form);
+
+  DisplaySearch(jsonData);
+  let totalPages = GetTotalPages(jsonData);
+
+  ShowPageBtns();
+  DisableBtns(totalPages);
+};
+
+async function FetchData(form) {
+  url = "https://pixabay.com/api/?key=33506024-4219960d4e66c6a76dff1b306&q=";
+  page = 1;
+  
+  let text = form.search.value;
+  let color = form.color_choise.value;
+
+  if (color != "") {
+    url += text + "&colors=" + color;
+  } else {
+    url += text;
+  }
+
+  url += `&per_page=10&page=${page}`;
+
+  let response = await fetch(url);
+  //Alla bilder
+  return await response.json();
 }
 
-async function FetchData(form, url){
-    let text = form.search.value;
-    let color = form.color_choise.value;
+function DisplaySearch(json) {
+  for (let pic of json.hits) {
+    let newElement = document.createElement("li");
 
-    if (color != "") {
-        url += text + '&colors=' + color;
-    }
-    else {
-        url += text;
-    }
+    let listPic = document.createElement("img");
+    let picTags = document.createElement("h3");
+    let picAuthor = document.createElement("p");
 
-    url += '&per_page=10';
-
-    let response = await fetch(url);
-    //Alla bilder
-    return await response.json();
-}
-
-function DisplayData(json){
-
-    for (let pic of json.hits) {
-
-        let newElement = document.createElement("li");
-
-        let listPic = document.createElement('img');
-        let picTags = document.createElement("h3");
-        let picAuthor = document.createElement("p");
-
-        listPic.src = pic.previewURL;
-        picTags = pic.tags;
-        picAuthor = `Taken by ${pic.user}`; newElement.append(listPic, picTags, picAuthor)
-        list.append(newElement);
-    }
+    listPic.src = pic.previewURL;
+    picTags = pic.tags;
+    picAuthor = `Taken by ${pic.user}`;
+    newElement.append(listPic, picTags, picAuthor);
+    list.append(newElement);
+  }
 }
 function ShowPageBtns() {
-    nextPage.style.display = "inline-block";
-    previousPage.style.display = "inline-block";
-}
-function GetTotalPages(jsonData) {
-    let pages = jsonData.totalHits;
-    return Math.ceil(pages / 10);
+  nextPage.style.display = "inline-block";
+  previousPage.style.display = "inline-block";
 }
 
 function RemoveOldSearch() {
-    while (list.firstElementChild) {
-        list.firstElementChild.remove();
-    }
+  while (list.firstElementChild) {
+    list.firstElementChild.remove();
+  }
 }
 
-nextPage.addEventListener("click", function(){
-    ChangePage(url, 1);
+nextPage.addEventListener("click", function () {
+  ChangePage(1);
 });
 
-async function ChangePage(url, pageChange){
+previousPage.addEventListener("click", function () {
+  ChangePage(-1);
+});
 
-    RemovePages();
+async function ChangePage(pageChange) {
+  event.preventDefault();
+  RemovePages();
 
-    url += page + pageChange;
+  url += parseInt(page) + parseInt(pageChange);
+  page = page + pageChange;
 
-    let jsonData = await FetchData(form, url);
+  let jsonData = await FetchNewPage(url);
+  RemoveOldSearch();
 
-    DisplayData(jsonData);
+  DisplaySearch(jsonData);
+  totalPages = GetTotalPages(jsonData);
+
+  DisableBtns(totalPages);
 }
-function RemovePages(){
-    let count = 0;
+async function FetchNewPage(url) {
+  let response1 = await fetch(url);
+  //Alla bilder
 
-    for (let index = url.length - 1; index > 0; index++) {
+  return await response1.json();
+}
 
-        let number = parseInt(url[index]);
+function RemovePages() {
+  let count = 0;
 
-        if(isNaN(number)){
-            count++;
-        }
+  for (let index = url.length - 1; index > 0; index++) {
+    let number = parseInt(url[index]);
+
+    if (!isNaN(number)) {
+      count++;
+    } else {
+      break;
     }
+  }
 
-    url = url.substring(0,url.length - count);
+  url = url.substring(0, url.length - count);
 }
 
-nextPage.onclick = async event => {
-
-    function NextPage(url, pageChange) {
-        page++;
-
-        url = url + '&page=' + page;
-    }
-    let response = await fetch(url);
-    //Alla bilder
-    let json = await response.json();
-
-
-    for (let pic of json.hits) {
-
-        let newElement = document.createElement("li");
-
-        let listPic = document.createElement('img');
-        let picTags = document.createElement("h3");
-        let picAuthor = document.createElement("p");
-
-        listPic.src = pic.previewURL;
-        picTags = pic.tags;
-        picAuthor = `Taken by ${pic.user}`; newElement.append(listPic, picTags, picAuthor)
-        list.append(newElement);
-
-    }
-
+function DisableBtns(totalPagees) {
+  if (page == 1) {
+    previousPage.disabled = true;
+  } else {
+    previousPage.disabled = false;
+  }
+  if (page == lastPage) {
+    nextPage.disabled = true;
+  } else {
+    nextPage.disabled = false;
+  }
 }
 
-
-function DisableBtns(jsonData) {
-    let lastPage = GetTotalPages(jsonData);
-
-    // if(page == )
-    // nextPage
-
+function GetTotalPages(jsonData) {
+  let pages = jsonData.totalHits;
+  return Math.ceil(pages / 10);
 }
+
+// nextPage.onclick = async event => {
+
+//     function NextPage(url, pageChange) {
+//         page++;
+
+//         url = url + '&page=' + page;
+//     }
+//     let response = await fetch(url);
+//     //Alla bilder
+//     let json = await response.json();
+
+//     for (let pic of json.hits) {
+
+//         let newElement = document.createElement("li");
+
+//         let listPic = document.createElement('img');
+//         let picTags = document.createElement("h3");
+//         let picAuthor = document.createElement("p");
+
+//         listPic.src = pic.previewURL;
+//         picTags = pic.tags;
+//         picAuthor = `Taken by ${pic.user}`; newElement.append(listPic, picTags, picAuthor)
+//         list.append(newElement);
+
+//     }
+
+// }
